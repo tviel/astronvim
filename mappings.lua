3d33abb34cd9cee8545ce150ddc14205d91b1f98
Thorsten Viel <thorsten.viel@gmx.de>
2023-07-31 16:58:00 +0200

    tab to navigate buffers

--- mappings.lua
+++ mappings.lua
@@ -27,6 +27,9 @@ return {
       end,
       desc = "Pick to close",
     },
+    ["<Tab>"] = { function() require("astronvim.utils.buffer").nav(vim.v.count > 0 and vim.v.count or 1) end, desc = "Next buffer" },
+    ["<S-Tab>"] = { function() require("astronvim.utils.buffer").nav(-(vim.v.count > 0 and vim.v.count or 1)) end,  desc = "Previous buffer" },
+
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
